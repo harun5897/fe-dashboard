1a52265973d623a5ffe7b70b9199e4fabbed965d
harun5897 <harungb025@gmail.com>
2023-03-27 18:47:36 +0700

    refactor(app): import all component to be page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
+import React, { useState, useEffect } from 'react'
+import NavigationMenu from "./components/navigation-menu/NavigationMenu.js"
+import Header from "./components/header/Header.js"
+import Home from "./pages/Home.js"
+
 function App() {
+  const [showMenuDesktop, setShowMenuDesktop] = useState(false)
+  const [marginPixel, setMarginPixel] = useState(295)
+  const [widthScreen, setWidthScreen] =  useState(window.innerWidth)
+  const [showPage, setShowPage] = useState('')
+  const [showMenu, setShowMenu] = useState('')
+
+  const hidePage = (val) => {
+    if(val === 'block') {
+      if(!showMenuDesktop) {
+        setShowMenuDesktop(true)
+        setShowMenu('hidden')
+        setMarginPixel(0)
+      } else {
+        setShowMenuDesktop(false)
+        setShowMenu('block')
+        setMarginPixel(295)
+      }
+    } else {
+      setShowPage(val)
+      setShowMenu('block')
+    }
+  }
+  const hideMenu = (val) => {
+    setShowMenu(val)
+    setShowPage('block')
+  }
+  
+  useEffect(()=> {
+    widthScreen < 1024 ? setShowMenu('hidden') : setShowMenu('block')
+  },[widthScreen, setWidthScreen])
+
   return (
-    <div className="App">
-      <h1>Hello React Js</h1>
+    <div className="App relative">
+      <div className={`fixed top-0 lg:z-0 z-20 ${ showMenu }`}>
+        <NavigationMenu onEvent={ hideMenu } />
+      </div>
+      <div className={`fixed w-full top-0 min-[1224px]:ml-[${marginPixel}px] ml-0 min-[1224px]:pr-[${marginPixel}px] pr-0 z-10 ${ showPage }`}>
+        <Header onEvent={ hidePage }  />
+      </div>
+      <div className={`min-[1224px]:ml-[${marginPixel}px] ml-0 ${ showPage }`}>
+        <Home />
+      </div>
     </div>
   )
 }
